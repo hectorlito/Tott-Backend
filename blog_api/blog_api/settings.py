433c592cf01a116fa776929fe8704f182a81ea80
hector guevara <hectorguevara@hectors-MBP.fios-router.home>
2019-01-10 16:41:52 -0500

    removed settings on line 14

--- blog_api/blog_api/settings.py
+++ blog_api/blog_api/settings.py
@@ -11,10 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import dj-database-url
+# import dj-database-url
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -93,7 +91,8 @@ DATABASES = {
     }
 }
 
-
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 

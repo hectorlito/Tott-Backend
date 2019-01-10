8b8da44f40e5979176ac6aa7f7b81dc3090d2c2e
hector guevara <hectorguevara@hectors-MBP.fios-router.home>
2019-01-10 14:32:42 -0500

    Changed procfile

--- blog_api/blog_api/settings.py
+++ blog_api/blog_api/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'xrg+$6@906)ygqx=g9*_lv^3(xrsu97g)ekjl^yd4m3salgxa4'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'top-back.herokuapp.com/']
 
@@ -127,9 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
-
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
@@ -138,3 +135,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

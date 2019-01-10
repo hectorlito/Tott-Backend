0d29788249ea33ff6a61af94e89fc89beba1a040
hector guevara <hectorguevara@hectors-MBP.fios-router.home>
2019-01-09 20:36:04 -0500

    added runtime file

--- blog_api/blog_api/settings.py
+++ blog_api/blog_api/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'xrg+$6@906)ygqx=g9*_lv^3(xrsu97g)ekjl^yd4m3salgxa4'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'top-back.herokuapp.com/']
 
 
 # Application definition
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
 
 
 REST_FRAMEWORK = {

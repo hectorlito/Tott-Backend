c248095917fc3efe8fec83fb1280f0f230b8d75d
hector guevara <hectorguevara@hectors-MBP.fios-router.home>
2019-01-13 19:21:12 -0500

    fm serializer added

--- blog_api/blog/views.py
+++ blog_api/blog/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 # Create your views here.
 
 from .models import Blog, FM
-from .serializers import BlogSerializer
+from .serializers import BlogSerializer, FMSerializer
 
 
 class ListBlog(generics.ListCreateAPIView):
